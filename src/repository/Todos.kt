480c39b5feeb6fefa8db1465e7e4fc77fba081fd
Fbada006 <ferdinandkamata@yahoo.com>
2022-08-18 18:05:04 +0300

    update versions and fix primary key problem

--- src/repository/Todos.kt
+++ src/repository/Todos.kt
@@ -4,8 +4,10 @@ import org.jetbrains.exposed.sql.Column
 import org.jetbrains.exposed.sql.Table
 
 object Todos: Table() {
-    val id : Column<Int> = integer("id").autoIncrement().primaryKey()
+    val id : Column<Int> = integer("id").autoIncrement()
     val userId : Column<Int> = integer("userId").references(Users.userId)
     val todo = varchar("todo", 512)
     val done = bool("done")
+
+    override val primaryKey = PrimaryKey(id, name = "TODO_User_ID")
 }
