5cb3864d28fc04e6853a621a4564b35cc69cc87d
Fbada006 <ferdinandkamata@yahoo.com>
2022-08-18 18:16:14 +0300

    fix testing issue

--- test/ApplicationTest.kt
+++ test/ApplicationTest.kt
@@ -1,23 +1,17 @@
 package com.disruption
 
-import io.ktor.http.HttpMethod
-import io.ktor.http.HttpStatusCode
-import io.ktor.locations.KtorExperimentalLocationsAPI
-import io.ktor.server.testing.handleRequest
-import io.ktor.server.testing.withTestApplication
-import io.ktor.util.KtorExperimentalAPI
-import kotlin.test.Test
-import kotlin.test.assertEquals
+import io.ktor.server.locations.*
+import io.ktor.server.testing.*
+import org.junit.Test
+
 
 class ApplicationTest {
-    @KtorExperimentalAPI
     @KtorExperimentalLocationsAPI
     @Test
     fun testRoot() {
-        withTestApplication({ module(testing = true) }) {
-            handleRequest(HttpMethod.Get, "/").apply {
-                assertEquals(HttpStatusCode.OK, response.status())
-                assertEquals("HELLO WORLD!", response.content)
+        testApplication {
+            application {
+
             }
         }
     }
