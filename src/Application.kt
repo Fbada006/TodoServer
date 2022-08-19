02a885565757dd3b21388a0ee9359b6a2a9d2f71
Fbada006 <ferdinandkamata@yahoo.com>
2022-08-19 13:03:49 +0300

    attemot deploy

--- src/Application.kt
+++ src/Application.kt
@@ -1,26 +1,23 @@
 package com.disruption
 
 import com.disruption.auth.JwtService
-import com.disruption.auth.MySession
 import com.disruption.plugins.configureRouting
 import com.disruption.plugins.configureSecurity
 import com.disruption.plugins.configureSerialization
 import com.disruption.repository.DatabaseFactory
 import com.disruption.repository.TodoRepository
-import io.ktor.server.application.*
 import io.ktor.server.engine.*
 import io.ktor.server.locations.*
 import io.ktor.server.netty.*
-import io.ktor.server.sessions.*
-import kotlin.collections.set
 
 @KtorExperimentalLocationsAPI
 fun main() {
-    DatabaseFactory.init()
+
     val db = TodoRepository()
     val jwtService = JwtService()
 
     embeddedServer(Netty, port = 8080, host = "0.0.0.0") {
+        DatabaseFactory.init()
         configureSecurity(db, jwtService)
         configureRouting(db, jwtService)
         configureSerialization()
