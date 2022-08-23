fe4a14bb53543b055639680ba7e47117cc2c8d9c
Fbada006 <ferdinandkamata@yahoo.com>
2022-08-23 17:09:17 +0300

    attempt fix for module error

--- src/Application.kt
+++ src/Application.kt
@@ -6,22 +6,23 @@ import com.disruption.plugins.configureSecurity
 import com.disruption.plugins.configureSerialization
 import com.disruption.repository.DatabaseFactory
 import com.disruption.repository.TodoRepository
+import io.ktor.server.application.*
 import io.ktor.server.engine.*
 import io.ktor.server.locations.*
 import io.ktor.server.netty.*
 
-@KtorExperimentalLocationsAPI
-fun main() {
+fun main(args: Array<String>): Unit = io.ktor.server.netty.EngineMain.main(args)
 
+@KtorExperimentalLocationsAPI
+@Suppress("unused") // Referenced in application.conf
+fun Application.module() {
     val db = TodoRepository()
     val jwtService = JwtService()
 
-    embeddedServer(Netty, port = System.getenv("PORT").toInt()) {
     DatabaseFactory.init()
-        configureSecurity(db, jwtService)
-        configureRouting(db, jwtService)
-        configureSerialization()
-    }.start(wait = true)
+    configureSecurity(db, jwtService)
+    configureRouting(db, jwtService)
+    configureSerialization()
 }
 
 const val API_VERSION = "/v1"
