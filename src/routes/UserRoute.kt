f46553283cb145c7e5fc6627b2b0e779aea37a7b
Fbada006 <ferdinandkamata@yahoo.com>
2022-08-23 20:25:40 +0300

    add user details route

--- src/routes/UserRoute.kt
+++ src/routes/UserRoute.kt
@@ -7,6 +7,7 @@ import com.disruption.repository.Repository
 import io.ktor.http.HttpStatusCode
 import io.ktor.http.Parameters
 import io.ktor.server.application.*
+import io.ktor.server.auth.*
 import io.ktor.server.locations.*
 import io.ktor.server.locations.post
 import io.ktor.server.request.*
@@ -18,6 +19,7 @@ import io.ktor.server.sessions.*
 const val USERS = "$API_VERSION/users"
 const val USER_LOGIN = "$USERS/login"
 const val USER_CREATE = "$USERS/create"
+const val USER_DETAILS = "$USERS/getuser"
 
 @KtorExperimentalLocationsAPI
 @Location(USER_LOGIN)
@@ -26,6 +28,10 @@ class UserLoginRoute
 @KtorExperimentalLocationsAPI
 @Location(USER_CREATE)
 class UserCreateRoute
+
+@KtorExperimentalLocationsAPI
+@Location(USER_DETAILS)
+class UserDetailsRoute
 
 @KtorExperimentalLocationsAPI
 // 1
@@ -68,10 +74,12 @@ fun Route.users(
         val signinParameters = call.receive<Parameters>()
         val password = signinParameters["password"]
             ?: return@post call.respond(
-                HttpStatusCode.Unauthorized, "Missing Fields: Password")
+                HttpStatusCode.Unauthorized, "Missing Fields: Password"
+            )
         val email = signinParameters["email"]
             ?: return@post call.respond(
-                HttpStatusCode.Unauthorized, "Missing Fields: Email")
+                HttpStatusCode.Unauthorized, "Missing Fields: Email"
+            )
         val hash = hashFunction(password)
         try {
             val currentUser = db.findUserByEmail(email)
@@ -81,12 +89,31 @@ fun Route.users(
                     call.respondText(jwtService.generateToken(currentUser))
                 } else {
                     call.respond(
-                        HttpStatusCode.BadRequest, "Problems retrieving User")
+                        HttpStatusCode.BadRequest, "Problems retrieving User"
+                    )
                 }
             }
         } catch (e: Throwable) {
             application.log.error("Failed to register user", e)
             call.respond(HttpStatusCode.BadRequest, "Problems retrieving User")
+        }
+    }
+
+    authenticate("auth-jwt") {
+        get<UserDetailsRoute> {
+            val userId = call.request.queryParameters["userid"]?.toInt()
+                ?: return@get call.respond(
+                    HttpStatusCode.BadRequest, "The user id cannot be missing in this request."
+                )
+
+            val user = db.findUser(userId)
+                ?: return@get call.respond(
+                    HttpStatusCode.BadRequest, "Problems retrieving user. Does this user exist?"
+                )
+
+            return@get call.respond(
+                HttpStatusCode.OK, user
+            )
         }
     }
 }
